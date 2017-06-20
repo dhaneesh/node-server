282ce3b3f5ba54f807253072e80cc9abacb8a613
dhaneesh <dhaneesh88@gmail.com>
2017-06-20 08:53:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+//
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('scremIt', (text) => {
+  return text.toUpperCase();
+});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle : "Home page",
+    subHead : "Welcome to home page bro"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle : "About Page",
+    subHead : "Welcome to about page bro",
+  });
+});
+
+app.listen(9001);
